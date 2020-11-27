ac8f03dfdca17ea3d34ad68b1e760b665d51ebfc
MrUnbelievable92 <mkalimon@gmx.de>
2020-11-28 00:36:46 +0100

    Formatting
    hmm

--- UnitTesting/UnitTestAttribute.cs
+++ UnitTesting/UnitTestAttribute.cs
@@ -2,18 +2,17 @@
 
 namespace DevTools
 {
-    /// <summary>
-    /// Methods must be static, parameterless and return a boolean, representing the test result.
-    /// </summary>
+    /// <summary>    Methods must be static, parameterless and return a boolean, representing the test result.    </summary>
+    
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
     public class UnitTestAttribute : Attribute
     {
         public UnitTestAttribute(params string[] categories)
         {
-            Categories = categories ?? Categories;
+            Categories = categories ?? new string[0];
         }
 
 
-        public string[] Categories { get; protected set; } = new string[0];
+        public string[] Categories { get; protected set; }
     }
 }
