e38fa9d7d87288e553e1a8e6ab10948d363c1e3e
MrUnbelievable92 <mkalimon@gmx.de>
2020-12-14 21:11:16 +0100

    Release as Unity Package

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.0.2")]
-[assembly: AssemblyFileVersion("0.1.0.2")]
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
