c261ab421524747b0f58799795bc06616e0e4c83
Maximilian Kalimon <mkalimon@gmx.de>
2020-12-23 02:18:53 +0100

    Beautify
    (And remove duplicate code)

--- Log.cs
+++ Log.cs
@@ -1,45 +1,27 @@
-﻿namespace DevTools
+namespace DevTools
 {
     unsafe public static class Log
     {
         private static char[] HexValues => new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 
 
-        public static string Bits(sbyte value, bool spaces = true)
+        public static string Bits(byte value, bool spaces = true)
         {
             char* result = stackalloc char[8];
 
             for (int i = 0; i < 8; i++)
             {
-                result[i] = (char)((((uint)value >> (7 - i)) & 1) + 48);
+                result[i] = (char)(((value >> (7 - i)) & 1) + 48);
             }
 
-            return spaces ? new string(result, 0, 8).Insert(4, " ") : new string(result, 0, 8);
+            return new string(result, 0, 8).Insert(4, spaces ? " " : string.Empty);
         }
-        public static string Bits(short value, bool spaces = true)
-        {
-            return Bits((sbyte)((uint)value >> 8), spaces) + (spaces ? " " : string.Empty) + Bits((sbyte)value, spaces);
-        }
-        public static string Bits(int value, bool spaces = true)
-        {
-            return Bits((short)((uint)value >> 16), spaces) + (spaces ? " " : string.Empty) + Bits((short)value, spaces);
-        }
-        public static string Bits(long value, bool spaces = true)
-        {
-            return Bits((uint)((ulong)value >> 32), spaces) + (spaces ? " " : string.Empty) + Bits((uint)value, spaces);
-        }
-
-        public static string Bits(byte value, bool spaces = true) => Bits((sbyte)value, spaces);
-        public static string Bits(ushort value, bool spaces = true) => Bits((short)value, spaces);
-        public static string Bits(uint value, bool spaces = true) => Bits((int)value, spaces);
-        public static string Bits(ulong value, bool spaces = true) => Bits((long)value, spaces);
 
         public static string Bits<T>(T value, bool spaces = true)
             where T : unmanaged
         {
             byte* address = (byte*)&value;
             int sizeInBytes = sizeof(T);
-
             string result = string.Empty;
 
             while (sizeInBytes != 0)
@@ -52,6 +34,7 @@
 
             return result;
         }
+
         public static string Bits(void* ptr, int bytes, bool spaces = true)
         {
 Assert.IsNotNull(ptr);
@@ -72,34 +55,16 @@ Assert.IsGreater(bytes, -1);
         }
 
 
-        public static string Hex(sbyte value)
+        public static string Hex(byte value)
         {
             return HexValues[(uint)(byte)value >> 4].ToString() + HexValues[value & 15].ToString();
         }
-        public static string Hex(short value)
-        {
-            return Hex((byte)((uint)value >> 8)) + Hex((byte)(value & 255));
-        }
-        public static string Hex(int value, bool spaces = true)
-        {
-            return Hex((ushort)((uint)value >> 16)) + (spaces ? " " : string.Empty) + Hex((ushort)(value & ushort.MaxValue));
-        }
-        public static string Hex(long value, bool spaces = true)
-        {
-            return Hex((uint)((ulong)value >> 32), spaces) + (spaces ? " " : string.Empty) + Hex((uint)(value & uint.MaxValue), spaces);
-        }
-
-        public static string Hex(byte value) => Hex((sbyte)value);
-        public static string Hex(ushort value) => Hex((short)value);
-        public static string Hex(uint value, bool spaces = true) => Hex((int)value, spaces);
-        public static string Hex(ulong value, bool spaces = true) => Hex((long)value, spaces);
 
         public static string Hex<T>(T value, bool spaces = true)
             where T : unmanaged
         {
             byte* address = (byte*)&value;
             int iterations = 0;
-
             string result = string.Empty;
 
             while (iterations != sizeof(T))
@@ -112,6 +77,7 @@ Assert.IsGreater(bytes, -1);
 
             return result;
         }
+
         public static string Hex(void* ptr, int bytes, bool spaces = true)
         {
 Assert.IsNotNull(ptr);
